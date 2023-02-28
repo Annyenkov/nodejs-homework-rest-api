53fab63b3fa9b67204f91e734ad68485106a7f40
Viktor Annyenkov <annyenkov@gmail.com>
2023-02-28 19:59:33 +0200

    test

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const app = require('./app')
+// const app = require('./app')
 
-const DB_HOST = "mongodb+srv://VIktor:0eoS1xK9NiiTg3Nv@cluster0.xy2d9uu.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+// const DB_HOST = "mongodb+srv://VIktor:0eoS1xK9NiiTg3Nv@cluster0.xy2d9uu.mongodb.net/contacts_reader?retryWrites=true&w=majority"
 
-mongoose.set('strictQuery', true)
+// mongoose.set('strictQuery', true)
 
-mongoose.connect(DB_HOST)
+// mongoose.connect(DB_HOST)
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
