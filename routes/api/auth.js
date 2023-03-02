60733c1faf59e8a516009e82fecb9124e8fca354
Viktor Annyenkov <annyenkov@gmail.com>
2023-03-02 22:49:46 +0200

    register

---
+++ routes/api/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const { validateBody} = require("../../middlewares");
+const { schemas } = require("../../models/user");
+
+const {register} = require("../../controllers/auth")
+
+
+router.post("/register", validateBody(schemas.registerSchema), register);
+
+module.exports = router;
