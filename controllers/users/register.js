43a0b7254a3685e56adc2442fe375d81097d8b17
Viktor Annyenkov <annyenkov@gmail.com>
2023-03-13 21:58:09 +0200

    hw05-avatars

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,15 +1,23 @@
 const bcrypt = require("bcrypt");
 const { ctrlWrapper, HttpError } = require("../../helpers");
 const { User } = require("../../models/user");
+const gravatar = require('gravatar');
+
 
 const register = async (req, res) => {
-  const { email, password} = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw HttpError(409, "Email in use")
   };
   const createHashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({...req.body, password: createHashPassword });
+  const avatarUrl = gravatar.url(email);
+  console.log(avatarUrl);
+  const newUser = await User.create({
+    ...req.body,
+    avatarURL: avatarUrl,
+    password: createHashPassword,
+  });
   res.json({
     user: {
       email: newUser.email,
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { validateBody, authenticate} = require("../../middlewares");
+const { validateBody, authenticate, upload} = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
-const { register, login, current, logout, updateSubscription} = require("../../controllers/users")
+const { register, login, current, logout, updateSubscription, updateAvatar} = require("../../controllers/users")
 
 
 router.post("/register", validateBody(schemas.registerSchema), register);
@@ -12,6 +12,7 @@ router.post("/login", validateBody(schemas.loginSchema), login);
 router.post("/logout", authenticate, logout)
 router.get("/current", authenticate, current);
 router.patch("/:userId/subscription", validateBody(schemas.subscriptionSchema), updateSubscription);
+router.patch("/avatars", authenticate, upload.single("avatarURL"), updateAvatar);
 
 
 module.exports = router;
